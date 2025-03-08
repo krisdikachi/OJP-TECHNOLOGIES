744096ac7be88bbd6e818be139cfc2e98588e484
krisdikachi <krisdikachi@gmail.com>
2025-03-08 08:09:10 +0100

    removed the bg opacity and changed the bg colour

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -102,7 +102,17 @@ const Contact: React.FC = () => {
             <header className="text-4xl text-center font-bold text-ojpPrimary">Locate Us</header>
             
 
-            
+            <div className=" mt-10">
+                <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d63488.38991042998!2d6.864330570710713!3d5.991381077435929!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sLocation%3A%20Umudim%2C%20Nnewi%2C%20Anambra%20State%2C%20Along%20Traffic%20Light%20!5e0!3m2!1sen!2sng!4v1741410517967!5m2!1sen!2sng" 
+                    width="100%" 
+                    height="450" 
+                    style={{ border: 0 }} 
+                    // allowFullScreen={true} 
+                    loading="lazy" 
+                    referrerPolicy="no-referrer-when-downgrade">
+                </iframe>
+            </div>
         </>
     );
 };
